da1233075c96a133eadd2a03c946085979f4b1d7
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-31 15:06:21 +0200

    updated

---
+++ 01.DisneyLandx/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01.DisneyLandx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budged = double.Parse(Console.ReadLine());
+            double moneyCollected = 0;
+            int months = int.Parse(Console.ReadLine());
+            bool PastFirstMonth = false;
+            for (int i = 1; i <= months; i++)
+            {
+                if (PastFirstMonth == false)
+                {
+                    PastFirstMonth = true;
+                }
+                else if (i % 2 != 0)
+                {
+                    moneyCollected -= moneyCollected * 0.16;
+                }
+                if (i % 4 == 0)
+                {
+                    moneyCollected += moneyCollected * 0.25;
+                }
+                moneyCollected += budged * 0.25;
+            }
+            if (moneyCollected >= budged)
+            {
+                Console.WriteLine($"Bravo! You can go to Disneyland and you will have {(moneyCollected - budged):f2}lv. for souvenirs.");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry. You need {(budged - moneyCollected):f2}lv. more.");
+            }
+        }
+    }
+}
---
+++ 02.ArcheryTournament/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ArcheryTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            string[] command = Console.ReadLine()
+                .Split("@", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            int points = 0;
+            while (command[0] != "Game over")
+            {
+                if (command[0] == "Shoot Left")
+                {
+                    int startIndex = int.Parse(command[1]);
+                    int length = int.Parse(command[2]);
+                    if (startIndex < targets.Count && startIndex >= 0)
+                    {
+                        for (int i = 1; i <= length; i++)
+                        {
+                            if (i == length)
+                            {
+                                if (targets[startIndex] - 5 < 0)
+                                {
+                                    targets[startIndex] -= 5;
+                                    points += targets[startIndex] + 5;
+                                    targets[startIndex] = 0;
+                                }
+                                else if (targets[startIndex] - 5 > 0)
+                                {
+                                    targets[startIndex] -= 5;
+                                    points += 5;
+                                }
+                            }
+                            startIndex--;
+                            if (startIndex < 0)
+                            {
+                                startIndex = targets.Count - 1;
+                            }
+                        }
+                    }
+                }
+                else if (command[0] == "Shoot Right")
+                {
+                    int startIndex = int.Parse(command[1]);
+                    int length = int.Parse(command[2]);
+                    if (startIndex < targets.Count && startIndex >= 0)
+                    {
+                        for (int i = 1; i <= length; i++)
+                        {
+                            if (i == length)
+                            {
+                                if (targets[startIndex] - 5 < 0)
+                                {
+                                    targets[startIndex] -= 5;
+                                    points += targets[startIndex] + 5;
+                                    targets[startIndex] = 0;
+                                }
+                                else if (targets[startIndex] - 5 > 0)
+                                {
+                                    targets[startIndex] -= 5;
+                                    points += 5;
+                                }
+                            }
+                            startIndex++;
+                            if (startIndex >= targets.Count)
+                            {
+                                startIndex = 0;
+                            }
+                        }
+                    }
+                }
+                else if (command[0] == "Reverse")
+                {
+                    targets.Reverse();
+                }
+                command = Console.ReadLine()
+                .Split("@", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+            Console.WriteLine(string.Join(" - ", targets));
+            Console.WriteLine($"Iskren finished the archery tournament with {points} points!");
+        }
+    }
+}
