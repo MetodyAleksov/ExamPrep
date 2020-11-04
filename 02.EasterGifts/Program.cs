9db34a88fb5688a4b6e1f45c7b4a8f53d4a744f5
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-11-04 18:11:43 +0200

    updated

---
+++ 02.EasterGifts/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.EasterGifts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> gifts = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            string[] command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            while (command[0] != "No" && command[1] != "Money")
+            {
+                if (command[0] == "OutOfStock")
+                {
+                    string item = command[1];
+                    for (int i = 0; i < gifts.Count; i++)
+                    {
+                        if (gifts[i] == item)
+                        {
+                            gifts[i] = "None";
+                        }
+                    }
+                }
+                else if (command[0] == "Required")
+                {
+                    string gift = command[1];
+                    int index = int.Parse(command[2]);
+                    if (index >= 0 && index < gifts.Count)
+                    {
+                        gifts[index] = gift;
+                    }
+                }
+                else if (command[0] == "JustInCase")
+                {
+                    gifts[gifts.Count - 1] = command[1];
+                }
+                command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+            gifts.RemoveAll(x => x == "None");
+            Console.WriteLine(string.Join(" ", gifts));
+        }
+    }
+}
---
+++ 03.EasterShopping/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace _03.EasterShopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shops = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                if (command[0] == "Include")
+                {
+                    shops.Add(command[1]);
+                }
+                else if (command[0] == "Visit")
+                {
+                    if (command[1] == "first")
+                    {
+                        int index = int.Parse(command[2]);
+                        if(0 + index < shops.Count)
+                        {
+                            shops.RemoveRange(0, index);
+                        }
+                    }
+                    else
+                    {
+                        int index = shops.Count - int.Parse(command[2]);
+                        if (index >= 0 && index < shops.Count)
+                        {
+                            shops.RemoveRange(index, int.Parse(command[2]));
+                        }
+                    }
+                }
+                else if (command[0] == "Prefer")
+                {
+                    int index1 = int.Parse(command[1]);
+                    int index2 = int.Parse(command[2]);
+                    if ((index1 >= 0 && index1 < shops.Count) && (index2 >= 0 && index2 < shops.Count))
+                    {
+                        string item1 = shops[index1];
+                        string item2 = shops[index2];
+                        shops[index1] = item2;
+                        shops[index2] = item1;
+                    }
+                }
+                else if (command[0] == "Place")
+                {
+                    int index = int.Parse(command[2]);
+                    if (index >= 0 && index < shops.Count)
+                    {
+                        shops.Insert(index + 1, command[1]);
+                    }
+                }
+            }
+            Console.WriteLine($"Shops left:\n{string.Join(" ", shops)}");
+        }
+    }
+}
